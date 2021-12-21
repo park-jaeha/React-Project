fff6e586f965eb0956c30784032b856e6ef7a95b
park-jaeha <jaeha1477@naver.com>
2021-12-21 14:53:17 +0900

    change path

--- src/App.js
+++ src/App.js
@@ -31,10 +31,10 @@ const App =() =>{
         </header>
         <div className='App'>
             <Router>
-                <Route exact path ="https://jh-blogtest.netlify.app/home" component ={Home} />
-                <Route exact path ="https://jh-blogtest.netlify.app/test" component ={Test} /> 
-                <Route path ="https://jh-blogtest.netlify.app/main" component ={Main}></Route>
-                <Route path ="https://jh-blogtest.netlify.app/1" component ={ControlledCarousel}></Route>
+                <Route exact path ="jh-blogtest.netlify.app/home" component ={Home} />
+                <Route exact path ="jh-blogtest.netlify.app/test" component ={Test} /> 
+                <Route path ="jh-blogtest.netlify.app/main" component ={Main}></Route>
+                <Route path ="jh-blogtest.netlify.app/1" component ={ControlledCarousel}></Route>
             </Router>
         </div>
         </>
