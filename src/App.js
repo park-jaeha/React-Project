f62c69f09e76523a86d091e5885020d25c8663cb
park-jaeha <jaeha1477@naver.com>
2021-12-22 14:32:39 +0900

    add templete

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React, { Component, useState, useEffect } from 'react';
 // import {Navbar, Nav, Container, Form, Button, FormControl, Jumbotron} from 'react-bootstrap';
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import { Axios } from 'axios';
 /** import Screens */
 import {Home, Main, Test} from './screens/DetailScreens/Index';
 import Navi from "./screens/DetailScreens/Nav";
+import Project from './views/Dashboard';
+import Map from './views/Maps';
+import TableList from './views/TableList';
 /** Component import */
 import ControlledCarousel from './screens/Component/ControlledCarousel';
 
@@ -31,10 +34,14 @@ const App =() =>{
         </header>
         <div className='App'>
             <Router>
-                <Route exact path ="/" component ={Home} />
-                <Route exact path ="/test" component ={Test} /> 
-                <Route path ="/main" component ={Main}></Route>
-                <Route path ="/1" component ={ControlledCarousel}></Route>
+                <Switch>
+                    <Route exact path ="/" component ={Home} />
+                    <Route exact path ="/test" component ={Test} /> 
+                    <Route path ="/main" component ={Main}></Route>
+                    <Route path ="/1" component ={ControlledCarousel}></Route>
+                    <Route path ="/2" component ={Project}></Route>
+                    <Route path ="/3" component = {TableList}/>
+                </Switch>
             </Router>
         </div>
         </>
