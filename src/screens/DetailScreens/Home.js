dfb1b0791ae55de1bcae492b7623060e1e3bf23a
park-jaeha <jaeha1477@naver.com>
2021-12-22 11:30:37 +0900

    navbar br delete

--- src/screens/DetailScreens/Home.js
+++ src/screens/DetailScreens/Home.js
@@ -4,13 +4,19 @@ import background from './../../Image/img1.jpg';
 const Home = () =>{
 
     return(
-        <div className="Home" style = {{backgroundImage:{background}}}>
-            <h3 style = {{textAlign:"center"}}>Welcome to JH's Blog</h3>
+        <div className="Home" 
+                style = {{
+                    // backgroundImage:"url("+background+")",backgroundPosition: 'center',
+                    // backgroundSize: 'cover',
+                    // backgroundRepeat: 'no-repeat',
+                    // opacity:0.5
+                }}>
             <img
-                style ={{height:"1000px", width : "100%", opacity:0.5}}
+                style ={{height:"800px", width : "100%", opacity:0.5}}
                 src={background}
                 alt="First slide"
-            />
+            /> 
+            <h3 style = {{textAlign:"center"}}>Welcome to JH's Blog</h3>
             {/* </img> */}
         </div>
     );
